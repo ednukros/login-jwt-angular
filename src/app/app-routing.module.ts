7649567a4e389efded7e64eeaf33e6f1d47ef0c6
ednukros <isanmenarguez@gmail.com>
2023-02-11 12:15:02 +0100

    Feat: login & register component / routing configured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { registerLocaleData } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: AppComponent, pathMatch: 'full'},
+  {path: "login", component: LoginComponent, pathMatch: 'full'},
+  {path: "", component: RegisterComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
